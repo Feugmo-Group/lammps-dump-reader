1b73436504a05d6243c5996de574b211312c11ec
Mafuoso <77584862+Mafuoso@users.noreply.github.com>
2024-05-07 15:10:02 -0400

    revamped read_whole_dump. New data structure, better usability for LAMMPS application Still need to add compatability for different boundary conditions and dump types

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
+#unDumped = [{'TIMESTEP':int, 'NUMBER OF ATOMS':int, 'BOX BOUNDS':dict specific to each boundary condition}, 'ATOMS':[dict specific to dump style]}]
+#pp dict: {'x':[x1,x2], 'y':[y1,y2].'z':[z1,z2]}
+#id type scaled positions {'id':str, 'type':str, 'xs':str, 'ys':str, 'zs':str}
 
-# general format of unDumped as of now {(id,type),(x,y,z)}
 
 
-#function which loads entire dump file into memory
-def read_whole_dump(file:str) -> dict:
-    """read the whole file into a dictionary and return it"""
-    #intializing variables
-    count = 0    
-    timeStep = []
-    unDumped = {}
+
+
+def read_whole_dump(file:str) -> list[dict[str:any]]:
+    """read the entire file into an unDumped data structure"""
+    lineCount = 0 #keeps track of what line we are in a given frame
+    frameCount = -1 #keeps track of what frame we are in, starts at =1 because the first frame is considered the 0th frame
+    unDumped = [] #intializing the final return object
     
     with open(file) as myfile:
         for line in myfile:
-            count += 1           #keeps track of what line we are on
-            if "TIMESTEP" in line: #resets count every timestep
-                count = 1
-            if count == 2:
-                timeStep.append(line) #appends each timeStep to a list, may use this later to create a new dict for each timeStep
-            elif count == 4:
-                atomCount = line      #stores atom count, does this change every time step? should it also be a list?
-            elif count == 6:
-                boundary_x = line.split() #boundary conditions coordinate x
-            elif count == 7:
-                boundary_y = line.split() ##boundary conditions coordinate y
-            elif count == 8:
-                boundary_z = line.split() #boundary conditions coordinate z
-            elif count >= 10:
-                s_line = line.split()     #splits lines at the whitespace
-                unDumped[(s_line[0],s_line[1])] = (s_line[2],s_line[3],s_line[4])  #creates the dict mentioned
+            lineCount += 1
+            if "TIMESTEP" in line: #how we know we are in a new frame
+                lineCount = 1 #resets the lineCount every frame
+                frameCount += 1 #updates the frameCount when we enter a new frame
+                unDumped.append({"TIMESTEP":0 , "NUMBER OF ATOMS":0 , "BOX BOUNDS":{} , "ATOMS": [] }) #skeleton of the final output data structure
+            if lineCount == 2: #The line in a frame that contains the time step information
+                unDumped[frameCount]["TIMESTEP"] = int(line) #updates the value of the TIMESTEP key
+            elif lineCount == 4:
+                unDumped[frameCount]["NUMBER OF ATOMS"] = int(line) #updates the value of the NUMBER OF ATOMS key
+            elif lineCount == 5: #the line that contains information about the boundary conditions
+                if "pp" in line: #logic that determines the format of the value of BOX BOUNDS
+                    unDumped[frameCount]["BOX BOUNDS"] = {"x":[], "y":[], "z":[]}
+            elif lineCount == 6: # lines that contain values of BOX BOUNDS, will be organized depending on boundary type
+                unDumped[frameCount]["BOX BOUNDS"]["x"] = line.split()
+            elif lineCount == 7:
+                unDumped[frameCount]["BOX BOUNDS"]["y"] = line.split()
+            elif lineCount == 8:
+                unDumped[frameCount]["BOX BOUNDS"]["z"] = line.split()
+            elif lineCount == 9: #line that contains information about the atom information being dumped, generates format of output dictionary 
+                v_line = line.split()
+            elif lineCount >= 10: #lines 10 and up contain dump data of each atom
+                s_line = line.split()
+                unDumped[frameCount]["ATOMS"].append({v_line[2]:s_line[0], v_line[3]:s_line[1], v_line[4]:s_line[2], v_line[5]:s_line[3], v_line[6]:s_line[4]}) #creating a dict specifiying each atom
+                
+    return unDumped
 
-    return unDumped           
 
-
+  
+                
+                
