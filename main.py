5fac87f6d5b29dc3d922481ec5b928dfa6230668
mafuoso <mu2faroo@uwaterloo.ca>
2024-05-08 14:12:18 -0400

    Revamped read_whole_dump to not rely on line count. read_whole_dump now uses logic to keep track of what information to put into dict as opposed to line number

--- main.py
+++ main.py
@@ -6,33 +6,41 @@
 
 def read_whole_dump(file: str) -> list[dict[str:any]]:
     """read the entire file into an unDumped data structure"""
-    lineCount = 0 #keeps track of what line we are in a given frame
     frameCount = -1 #keeps track of what frame we are in, starts at =1 because the first frame is considered the 0th frame
     unDumped = [] #intializing the final return object
-    
-    with open(file) as myfile:
+    loopCount = 0 #intializes the frame loop counter
+
+    with open(file, "r") as myfile:
         for line in myfile:
-            lineCount += 1
+            loopCount += 1
             if "TIMESTEP" in line: #how we know we are in a new frame
-                lineCount = 1 #resets the lineCount every frame
+                loopCount = 0 #keeps track of how many loops we have gone through in a frame
+                mode = "timeStep" #resets the lineCount every frame
+                checkLoop = loopCount
                 frameCount += 1 #updates the frameCount when we enter a new frame
-                unDumped.append({}) #skeleton of the final output data structure
-            if lineCount == 2: #The line in a frame that contains the time step information
+                unDumped.append({"ATOMS":[]}) #skeleton of the final output data structure
+            if mode == 'timeStep' and (loopCount - checkLoop) == 1: #goes to the line after the one with the timestep title
                 unDumped[frameCount]["TIMESTEP"] = int(line) #updates the value of the TIMESTEP key
-            elif lineCount == 4:
+            if "NUMBER OF ATOMS" in line: #checks for the line with number of atoms title
+                mode = "atomNum"
+                checkLoop = loopCount #stores the current loop number
+            if mode == "atomNum" and (loopCount - checkLoop) == 1: # grabs the line with the Number of atoms information
                 unDumped[frameCount]["NUMBER OF ATOMS"] = int(line) #updates the value of the NUMBER OF ATOMS key
-            elif lineCount == 5: #the line that contains information about the boundary conditions
-                if "pp" in line: #logic that determines the format of the value of BOX BOUNDS
-                    unDumped[frameCount]["BOX BOUNDS"] = {"x":[], "y":[], "z":[]}
-            elif lineCount == 6: # lines that contain values of BOX BOUNDS, will be organized depending on boundary type
+            if "BOX BOUNDS" in line: #the line that contains information about the boundary conditions type
+                mode = "bounds"
+                checkLoop = loopCount
+                unDumped[frameCount]["BOX BOUNDS"] = {"x":[], "y":[], "z":[]}
+            if mode == "bounds" and (loopCount - checkLoop) == 1: 
                 unDumped[frameCount]["BOX BOUNDS"]["x"] = line.split()
-            elif lineCount == 7:
+            if mode == "bounds" and (loopCount - checkLoop) == 2: # lines that contain values of BOX BOUNDS, will be organized depending on boundary type
                 unDumped[frameCount]["BOX BOUNDS"]["y"] = line.split()
-            elif lineCount == 8:
+            if mode == "bounds" and (loopCount - checkLoop) == 3:
                 unDumped[frameCount]["BOX BOUNDS"]["z"] = line.split()
-            elif lineCount == 9: #line that contains information about the atom information being dumped, generates format of output dictionary 
+            if "id" in line: #line that contains information about the atom information being dumped, generates format of output dictionary 
+                mode = "atom"
+                checkLoop = loopCount
                 v_line = line.split() #store all the parameters in a list
-            elif lineCount >= 10: #lines 10 and up contain dump data of each atom
+            if mode == "atom" and (loopCount - checkLoop) >= 1: #lines 10 and up contain dump data of each atom
                 s_line = line.split() #store all the values in a list
                 paraCount = 0 #intializing a parameter count that keeps track of how many parameters specifty an atom
                 atomDict = {} #creating an empty dict for every new atom
